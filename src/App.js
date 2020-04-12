576abfd5edd3263dda01422b6b3407243ddff6ce
Daniel Franklin <dfranklin@fiern.org.br>
2020-04-11 21:36:19 -0300

    should be able to add new repository: Para que esse teste passe, sua aplicação deve permitir que um repositório seja adicionado ao seu backend e listado no seu frontend dentro de uma LI. should be able to remove repository: Para que esse teste passe, sua aplicação deve permitir que ao clicar no botão de remover que vai estar dentro da LI do repositório adicionado, o item seja removido da listagem.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [ repos, setRepos ] = useState([]);
+
+  useEffect( loadRepos, [] );
+
+  function loadRepos(){
+    api.get('/repositories').then(response => {
+      setRepos(response.data);
+    });
+  }
+  
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories', { title: `Novo Repositório ${Date.now()}` });
+    const repo = response.data;
+    setRepos( [...repos, repo] );
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);        
+    const new_repo = repos.filter( repo => repo.id !== id);
+    setRepos(new_repo);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repos.map( (repo, index ) => ( 
+        <li key={repo.id}>
+          {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
